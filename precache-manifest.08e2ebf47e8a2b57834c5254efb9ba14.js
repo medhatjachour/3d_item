1266a57fd9aef574736918ab65c6f78cfaf0d159
medhatjachour <53466991+medhatjachour@users.noreply.github.com>
2022-04-06 20:05:44 +0200

    Updates

---
+++ precache-manifest.08e2ebf47e8a2b57834c5254efb9ba14.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "55f1d26ffbf5f49c7fa2",
+    "url": "/mazboot/static/css/main.881f0994.chunk.css"
+  },
+  {
+    "revision": "55f1d26ffbf5f49c7fa2",
+    "url": "/mazboot/static/js/main.55f1d26f.chunk.js"
+  },
+  {
+    "revision": "f48ed7d98bc049281a4b",
+    "url": "/mazboot/static/js/1.f48ed7d9.chunk.js"
+  },
+  {
+    "revision": "f60de3c5f0732aa8a3dd",
+    "url": "/mazboot/static/css/2.e8b2544d.chunk.css"
+  },
+  {
+    "revision": "f60de3c5f0732aa8a3dd",
+    "url": "/mazboot/static/js/2.f60de3c5.chunk.js"
+  },
+  {
+    "revision": "77ba042d05bdd2c59c03",
+    "url": "/mazboot/static/js/runtime~main.77ba042d.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/mazboot/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/mazboot/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/mazboot/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/mazboot/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/mazboot/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "868819ec97a022e4204ce231707efa77",
+    "url": "/mazboot/index.html"
+  }
+];
